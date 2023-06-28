b515f222250ea54c631cc59c59140f258e45bc08
latikou <latifakouhaili469@gmail.com>
2023-06-28 06:16:01 -0700

    tasks done

--- environ.c
+++ environ.c
@@ -34,13 +34,13 @@ char *_getenv(info_t *info, const char *name)
 }
 
 /**
- * _myetenv - Initialide a new environment variable,
+ * _mysetenv - Initialise a new environment variable,
  *           or modify an existing one
  * @info: Structure containing potential arguments. Used to maintain
  *      constant function prototype.
  * Return: Always 0
  */
-int _myetenv(info_t *info)
+int _mysetenv(info_t *info)
 {
 	if (info->argc != 3)
 	{
@@ -74,7 +74,7 @@ int _myunsetenv(info_t *info)
 
 /**
  * populate_env_list - populates env linked list
- * @info: Structure containing potentail arguments. Used to maintain
+ * @info: Structure containing potential arguments. Used to maintain
  *    constant function prototype.
  * Return: Always 0
  */
